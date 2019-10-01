edff8e619f0166c40dd4979251eed3a3f2fb02ea
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-01 15:02:11 -0700

    added custom admin:

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    """Custom user admin to override the ordering and list display."""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+"""Package testing our admin functions."""
+
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Custom test class, testing our admin page and functionality."""
+
+    def setUp(self):
+        """Creates a test client, superuser that's logged in, and a user.
+
+        :return: sets class' self.client variable to new user
+        """
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@gmail.com",
+            password="password123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@gmail.com",
+            password="password123",
+            name="test user Full Name"
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page.
+
+        :return: None
+        :raises AssertionError: user doesn't have a name or email
+        """
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
