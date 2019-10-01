153dbed133fcffb30e573c911ecbdafe8a98576e
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-01 14:03:42 -0700

    added to .gitignore, TDD for calc

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+def add(x: int, y: int) -> int:
+    """Add two numbers together.
+
+    :param x: first number to add
+    :param y: second number to add
+    :return: sum of first number and second number
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Subtract two numbers from each other.
+
+    :param x: first number to be reduced
+    :param y: number to reduce the first term by
+    :return: subtraction of second term from first term
+    """
+    return x - y
