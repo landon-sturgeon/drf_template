442eda063237f89f8d2f4c941e5f34ff3a6a5ddc
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-02 15:06:36 -0700

    refactored tests and views

--- app/tag/views.py
+++ app/tag/views.py
@@ -6,53 +6,39 @@ from core.models import Tag, Child
 from . import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manages tags in the database."""
+class BaseApiAttrViewSet(viewsets.GenericViewSet,
+                         mixins.ListModelMixin,
+                         mixins.CreateModelMixin):
+    """Base view set for user owned API endpoints."""
 
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only.
+
+        :return: all the objects associated with the authenticated user
+        """
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new object
+
+        :param serializer: validation serializer used to create the object
+        :return: None
+        """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseApiAttrViewSet):
+    """Manages tags in the database."""
+
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        """Returns objects for the current authenticated user only.
 
-        :return: Returns only authenticated user information
-        """
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-    def perform_create(self, serializer):
-        """Create a new tag.
-
-        :param serializer: validation serializer used to create the tag
-        :return: None
-        """
-        serializer.save(user=self.request.user)
-
-
-class ChildViewSet(viewsets.GenericViewSet,
-                   mixins.ListModelMixin,
-                   mixins.CreateModelMixin):
+class ChildViewSet(BaseApiAttrViewSet):
     """Manages children in the database."""
 
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
     queryset = Child.objects.all()
     serializer_class = serializers.ChildSerializer
-
-    def get_queryset(self):
-        """Returns objects for the current authenticated user only.
-
-        :return: Returns only authenticated children information
-        """
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-    def perform_create(self, serializer):
-        """Create a new child object.
-
-        :param serializer: validation serializer used to create the child
-        :return: None
-        """
-        serializer.save(user=self.request.user)
